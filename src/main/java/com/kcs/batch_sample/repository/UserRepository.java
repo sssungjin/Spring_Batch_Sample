944b068cf4cbe1b15d90fcbdb7ae37375d50c2da
sssungjin <dkdlwls576@gmail.com>
2024-09-11 16:37:00 +0900

    refactor: repository annotation

--- src/main/java/com/kcs/batch_sample/repository/UserRepository.java
+++ src/main/java/com/kcs/batch_sample/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.kcs.batch_sample.repository;
 
 import com.kcs.batch_sample.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
